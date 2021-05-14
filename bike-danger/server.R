888c9b88c4eee0bb8957f51f5ab8e9e62bb9822b
Andrew Dyck <andrew@andrewdyck.com>
2021-05-14 16:10:46 +0000

    final commit

--- bike-danger/server.R
+++ bike-danger/server.R
@@ -10,51 +10,71 @@
 library(shiny)
 library(leaflet)
 library(mapsapi)
+library(dplyr)
+library(sf)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    # points <- eventReactive(input$recalc, {
-    #     cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-    # }, ignoreNULL = FALSE)
+    
+    accidents <- readr::read_csv('https://raw.githubusercontent.com/andrewjdyck/sask-bike-collisions/master/data/regina.csv') %>%
+        select('lat', 'lon') %>%
+        filter(!is.na(lat)) %>%
+        rename(long = lon)
     
     key <- Sys.getenv('GAPPS_KEY')
     
-    doc = mp_directions(
-        # origin = c(34.81127, 31.89277),
-        origin = c(-104.6189, 50.4452),
-        # destination = "Haifa",
-        destination = "Beaks Chicken",
-        alternatives = FALSE,
-        key = key,
-        quiet = TRUE
-    )
+    # text_reactive <- eventReactive( input$submit, {
+    #     origin = input$routeBegin
+    #     destination = input$routeEnd
+    #     origin
+    # })
     
-    r = mp_get_routes(doc)
-    
-    pnt <- c(-104.6189, 50.4452)
-    point <- t(as.matrix(pnt))
-    
-    output$mymap <- renderLeaflet({
+    output$mymap <- renderLeaflet(
         leaflet() %>%
             addProviderTiles(providers$Stamen.TonerLite,
                              options = providerTileOptions(noWrap = TRUE)
             ) %>%
-            # addMarkers(data = point)
-            # addMarkers(lat=50.4452, lng=-104.6189)
-            addPolylines(data = r, opacity = 1, weight = 7)
+            setView(lng=-104.6189, lat = 50.4452, zoom = 14) %>%
+            addMarkers(data=accidents, clusterOptions = markerClusterOptions())
+    )
+    
+    # respond to the filtered data
+    observeEvent( input$submit, {
+        
+        doc = mp_directions(
+            # origin = c(-104.6189, 50.4452),
+            origin = input$routeBegin,
+            # destination = "Beaks Chicken",
+            destination = input$routeEnd,
+            alternatives = FALSE,
+            mode="bicycling",
+            key = key,
+            quiet = TRUE
+        )
+        
+        rt = mp_get_routes(doc)
+        
+        viewCenter <- as.data.frame(st_coordinates(st_centroid(rt)))
+        
+        leafletProxy(mapId = "mymap")  %>%
+            addProviderTiles(providers$Stamen.TonerLite,
+                             options = providerTileOptions(noWrap = TRUE)
+            ) %>%
+            setView(lng=viewCenter$X, lat = viewCenter$Y, zoom = 13) %>%
+            addPolylines(data = rt, opacity = 1, weight = 7, layerId = 'bikeroute') %>%
+            addMarkers(data=accidents, clusterOptions = markerClusterOptions())
+
     })
 
 })
 
 
 
-# 
-# accidents <- data.frame(
-#     # id = c(1,2,3,4,5),
-#     lat = c(50.447218, 50.446684, 50.447723, 50.447238, 50.449212),
-#     long = c(-104.606566, -104.618063, -104.618042, -104.617409, -104.606541)
-# )
+# accidents <- readr::read_csv('https://raw.githubusercontent.com/andrewjdyck/sask-bike-collisions/master/data/regina.csv') %>%
+#     select('lat', 'lon') %>%
+#     filter(!is.na(lat)) %>%
+#     rename(long = lon)
+
 # kk <- kmeans(accidents, 3)
 # 
 # 
@@ -66,9 +86,14 @@ shinyServer(function(input, output) {
 #     markerColor = 'red'
 # )
 # 
-# leaflet() %>% 
+
+# viewCenter <- as.data.frame(st_coordinates(st_centroid(r)))
+# 
+# leaflet() %>%
 #     addProviderTiles("CartoDB.DarkMatter") %>%
-#     # addPolylines(data = r, opacity = 1, weight = 7, color = ~pal(alternative_id))
+#     setView(lng=viewCenter$X, lat = viewCenter$Y, zoom = 14) %>%
+#     addPolylines(data = r, opacity = 1, weight = 7, color = ~pal(alternative_id)) %>%
 #     # addMarkers(data=accidents) %>%
 #     # addMarkers(data=as.data.frame(kk$centers), icon = centerIcons)
 #     addMarkers(data=accidents, clusterOptions = markerClusterOptions())
+#     
--- bike-danger/ui.R
+++ bike-danger/ui.R
@@ -9,13 +9,37 @@
 
 library(shiny)
 library(leaflet)
+library(bslib)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("Bike danger in Regina, SK, Canada"),
+    theme = bs_theme(version = 4, bootswatch = "minty"),
     
-    leafletOutput("mymap"),
+    # Application title
+    titlePanel("Find a safe bicycle route in Regina, SK, Canada"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            textInput("routeBegin", label="Trip start", value="City Hall, Regina, SK"),
+            textInput("routeEnd", label="Trip endpoint", value="Beaks Chicken"),
+            actionButton("submit", label = "Map your route!"),
+            tags$br(),
+            p("Bicycling in a city can be dangerous at the best of times. This app helps you identify how safe your bicycle trip will be in Regina, SK, Canada by showing where your route might intersect with locations where previous bicycle-automobile accidents have occurred."),
+            p("Some examples of routes you may want to try are:"),
+            tags$ul(
+                tags$li("Wascana Lake to Mosaic Stadium"),
+                tags$li("Sandra Schmirler Leisure Centre to Regina City Hall")
+            )
+        ),
+        mainPanel(
+            leafletOutput("mymap")
+        )
+    ),
+    HTML("<br />"),
+    fluidRow(
+        style="float: right;",
+        p(HTML(paste("Made with love by ", a(href="http://github.com/andrewjdyck", "github.com/andrewjdyck"))))
+    )
 
 ))
